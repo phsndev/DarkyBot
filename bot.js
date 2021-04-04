c7b32b806202a9ab97c70085f5061d56ca6b6434
phsndev <78909025+phsndev@users.noreply.github.com>
2021-04-04 13:16:43 -0300

    Ping Pong Test
    Using Ping Pong to test if bot is responding in a Discord channel.

--- bot.js
+++ bot.js
@@ -5,12 +5,13 @@ const client = new Discord.Client()
  
 
 client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
 
-  console.log(`Logged in as ${client.user.tag}!`)
-
-})
-
-
- 
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
 
 client.login('NzQ2NTAwMTIwNjA2MTQ2NjYw.X0BOag.ZZvN5fYF3O_aeKQcYCtYm_aGgI4')
